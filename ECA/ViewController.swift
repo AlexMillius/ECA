425c27b7977ca3f0a7998d56fe2c61497cdc6d8d
Mohamed Lee <Mohamed@air-de-mohamed.fritz.box>
2016-04-20 14:16:53 +0200

    Firebase connecté

--- ECA/ViewController.swift
+++ ECA/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    let refFirebase = Firebase(url: "https://fiery-torch-1961.firebaseio.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refFirebase.setValue("Coucou Mon amour, j'ai configuré la base de donnée pour l'espace culturel :)")
     }
 
     override func didReceiveMemoryWarning() {
