c71ba5c2d7886f4485f1788513a0dd557c757054
Mohamed Lee <Mohamed@air-de-mohamed.fritz.box>
2016-04-22 16:45:54 +0200

    calcul automatique du nombre de mois à afficher ok

--- ECA/ViewController.swift
+++ ECA/ViewController.swift
@@ -15,8 +15,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var events = [basicEvent]()
     var currentDate: NSDate?
-    var currentDescription: String?
+    var currentDescription: String? //TODO: delete
     let dataTransfert = DataTransfert()
+    var numberOfMonthToDisplay = Int()
+    var numberOfEventInEachSection = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +34,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func eventHasBeenRetreive(events: [basicEvent]) {
         self.events = events
+        
+        
+        getNumberOfMonthToDisplay(events)
+        
+        
         eventTableView.reloadData()
+    }
+    
+    // calcul combien il y a de mois afin de pouvoir séparer les chapitres dans la tableView
+    func getNumberOfMonthToDisplay(events: [basicEvent]){
+        var refMois = String()
+        numberOfMonthToDisplay = Int()
+        for event in events {
+            var tempMois = String()
+            tempMois = event.moisToDisplay
+            print("\(refMois) != \(tempMois)")
+            if refMois != tempMois {
+                numberOfMonthToDisplay += 1
+                print("numberOfMonth: \(numberOfMonthToDisplay)")
+                refMois = tempMois
+            }
+        }
+    }
+    
+    // calcul combien il y a d'events par mois pour séparer les header de la tableView
+    func createArrayOfDataInSections(events: [basicEvent]){
+        
     }
     
     //MARK: tableView delegate
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //print("events.count: \(events.count)")
         return events.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return numberOfMonthToDisplay
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
